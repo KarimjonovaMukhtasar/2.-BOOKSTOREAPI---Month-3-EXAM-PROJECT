a89c5ad46dc5161a48b7d75c9e1c4e8094756403
KarimjonovaMukhtasar <karimjonovamukhtasar2003@gmail.com>
2025-11-16 14:05:59 +0500

    feat: bookController along with BookService added

---
+++ src/controllers/book.controller.js
@@ -0,0 +1,54 @@
+import { BookService } from "../services/books.service.js";
+
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await BookService.getAll();
+    return res.status(200).json(books);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+const getBookById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await BookService.getById(id);
+    if (!book) return res.status(404).json({ message: "Book not found" });
+    return res.status(200).json(book);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const createBook = async (req, res) => {
+  try {
+    const newBook = await BookService.create(req.validatedData);
+    return res.status(201).json(newBook);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updated = await BookService.update(id, req.body);
+    if (!updated) return res.status(404).json({ message: "Book not found" });
+    return res.status(200).json(updated);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+ const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await BookService.remove(id);
+    if (!deleted) return res.status(404).json({ message: "Book not found" });
+    return res.status(200).json({ message: "Book deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+export {getAllBooks, getBookById, createBook, updateBook, deleteBook}
